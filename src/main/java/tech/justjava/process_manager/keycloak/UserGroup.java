2499515e70c4c7abba01248d65a315ff43739aa6
Ooreoluwa Ibitowa <ooreoluwaibitowa@gmail.com>
2025-07-23 01:43:02 +0100

    user Management - BE

---
+++ src/main/java/tech/justjava/process_manager/keycloak/UserGroup.java
@@ -0,0 +1,49 @@
+package tech.justjava.process_manager.keycloak;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@RequiredArgsConstructor
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserGroup {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String  groupId;
+    @Column(unique=true)
+    private String groupName;
+    private String description;
+    private Integer members = 0;
+
+    public String getGroupName() {
+        if (groupName == null || groupName.isEmpty())
+            return groupName;
+        groupName = groupName.toLowerCase();
+        return groupName.substring(0, 1).toUpperCase() + groupName.substring(1);
+    }
+
+    public String getDescription() {
+        if (description == null || description.isEmpty())
+            return "No description provided";
+        return description;
+    }
+
+    public Integer getMembers() {
+        return members!=null?members:0;
+    }
+}
--- src/main/java/tech/justjava/process_manager/userManagement/UserManagementController.java
+++ src/main/java/tech/justjava/process_manager/userManagement/UserManagementController.java
@@ -1,24 +1,58 @@
 package tech.justjava.process_manager.userManagement;
 
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import tech.justjava.process_manager.keycloak.KeycloakService;
+import tech.justjava.process_manager.keycloak.UserDTO;
+import tech.justjava.process_manager.keycloak.UserGroup;
+
+import java.util.List;
+import java.util.Map;
 
 @Controller
+@RequiredArgsConstructor
 @RequestMapping("/users")
 public class UserManagementController {
+
+    private final KeycloakService keycloakService;
     @GetMapping
-    public String getUsers(){
+    public String getUsers(Model model){
+        List<UserDTO> users = keycloakService.getUsers();
+        model.addAttribute("users", users);
+
       return "userManagement/list";
-    };
+    }
 
     @GetMapping("/new")
-    public String addUser(){
+    public String addUser(Model model) {
+        List<UserGroup> userGroup = keycloakService.getUserGroups();
+        model.addAttribute("userGroups", userGroup);
+
         return "userManagement/create";
     }
     @GetMapping("/groups")
-    public String manageGroups(){
+    public String manageGroups(Model model){
+        List<UserGroup> userGroup = keycloakService.getUserGroups();
+        model.addAttribute("userGroups", userGroup);
         return "userManagement/groupManagement";
+    }
+
+    @PostMapping("/createUser")
+    public ResponseEntity<Void> createUser(@RequestParam Map<String, String> params){
+        keycloakService.createUserInGroup(params);
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/createGroup")
+    public ResponseEntity<Void> createGroup(@RequestParam Map<String, String> params){
+        keycloakService.createGroup(params.get("groupName"), params.get("groupDescription"));
+        return ResponseEntity.ok().build();
     }
 }
